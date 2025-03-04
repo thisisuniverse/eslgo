11f9255bb3f56e575e945b5a73df6a8ed9cddf5d
Jon Leren Schøpzinsky <jos@teleguys.com>
2025-03-04 16:29:02 +0100

    Update helper.go

--- helper.go
+++ helper.go
@@ -18,8 +18,8 @@ import (
 	"log"
 	"time"
 
-	"github.com/percipia/eslgo/command"
-	"github.com/percipia/eslgo/command/call"
+	"github.com/thisisuniverse/eslgo/command"
+	"github.com/thisisuniverse/eslgo/command/call"
 )
 
 func (c *Conn) EnableEvents(ctx context.Context) error {
